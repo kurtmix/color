9d2eb53ba80097949b8b1a96a08f4005c6aeec54
kurtmix <kurtmix123@gmail.com>
2016-04-05 21:15:43 +0000

    fscmmit

---
+++ colorchange.js
@@ -0,0 +1,7 @@
+$(document)function(){
+    var color = ['aqua', 'black', 'blue', 'fuchsia', 'gray', 'green', 
+'lime', 'maroon', 'navy', 'olive', 'orange', 'purple', 'red', 
+'silver', 'teal', 'white', 'yellow'];
+    var newDiv = "<div>"
+    newDiv.css("background","blue")
+};
