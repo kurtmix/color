a1cbe5162b16859ac533e61e2c8ba4876ee5d8ca
kurtmix <kurtmix123@gmail.com>
2016-04-14 21:15:43 +0000

    hey

--- colorchange.js
+++ colorchange.js
@@ -1,7 +1,25 @@
-$(document)function(){
-    var color = ['aqua', 'black', 'blue', 'fuchsia', 'gray', 'green', 
-'lime', 'maroon', 'navy', 'olive', 'orange', 'purple', 'red', 
-'silver', 'teal', 'white', 'yellow'];
-    var newDiv = "<div>"
-    newDiv.css("background","blue")
-};
+$(document).ready(function(){
+    var colorList = [
+    'black', 
+    'blue',
+    'gray',
+    'green',
+    'orange',
+    'purple',
+    'red',
+    'white',
+    'pink'
+    ];
+    for(i=0; i < colorList.length; i++){
+    var myDiv = $("<div>");
+    myDiv.addClass("color-box");
+    myDiv.css("background-color", colorList[i]);
+    $("body").append(myDiv);
+ }
+    $("#ed").click(function(){ 
+    for (i = 0; i < colorList.length; i++){
+      $("body").Math.random(colorList.length);
+    }
+    });
+ 
+});
